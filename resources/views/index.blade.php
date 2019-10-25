1ffd68b41fdb13c4acf3c5a2b3515e2b563916ec
bryceandy <bryceandy@rocketmail.com>
2019-10-25 15:48:49 +0300

    Add laravel mix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ 'css/styles.css' }}">
+    <link rel="stylesheet" href="{{ 'css/app.css' }}">
     <title>Document</title>
 </head>
 <body>
@@ -14,8 +14,8 @@
     @foreach( $posts as $post)
 
         <div class="post">
-            <h2 class="name">{{$post->name}}</h2>
-            <p class="body">{{$post->body}}</p>
+            <h2 class="name"><a href="post/{{ $post->name }}">{{ $post->name }}</a></h2>
+            <p class="body">{{ $post->body }}</p>
         </div>
 
     @endforeach
