ad65a10b026502136d0e66058742d5153eb2af28
bryceandy <bryceandy@rocketmail.com>
2019-12-12 21:52:21 +0300

    Clean comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,19 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\Http\Requests\CreateCommentRequest;
 use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
     // Save comment in the db
-    public function save(Post $post)
+    public function save(CreateCommentRequest $request, Post $post)
     {
-        $this->validate(request(), ['body' => 'required|min:5']);
-
         Comment::create([
-            'user_id' => auth()->id(),
+            'user_id' => Auth::id(),
             'post_id' => $post->id,
-            'body' => request()->body,
+            'body' => $request['body'],
         ]);
         return back()->with(['comment_success' => 'Your comment was saved']);
     }
