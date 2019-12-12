f5639a39d2231d2b045574a4b00ea82d70cef73f
bryceandy <bryceandy@rocketmail.com>
2019-12-12 21:03:19 +0300

    Add fixed padding

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,7 +22,7 @@
             @if( count($post->comments))
                 <h3>All comments</h3>
                 @foreach($post->comments as $comment)
-                    <div style="border:1px solid;margin:2%;padding:2vw;border-radius:4px">
+                    <div style="border:1px solid lightgrey;margin:1%;padding:1vw;border-radius:4px">
                         <i style="float:right;font-size:small">{{ $comment->user->first_name }} - {{ $comment->created_at->diffForHumans() }}</i>
                         <p style="margin-top:20px">{{ $comment->body }}</p>
                     </div>
