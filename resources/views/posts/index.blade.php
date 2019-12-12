3e9c6b93422debab9e024fc2abf327119dd8d4fb
bryceandy <bryceandy@rocketmail.com>
2019-12-12 12:40:20 +0300

    Change index display and remove Carbon class

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,14 +9,21 @@
 @endsection
 
 @section('content')
-    <h1 class="text-center">Blog Posts</h1>
+    <h1 class="text-center mb-4">Blog Posts</h1>
 
     @if( count($posts))
         @foreach( $posts as $post)
-            <div class="post">
-                <h2 class="name"><a href="post/{{ $post->slug }}">{{ $post->title }}</a></h2>
-                <i>by {{ $post->user->first_name }} {{ \Illuminate\Support\Carbon::parse($post->created_at)->diffForHumans() }}</i>
-                <div class="body">{!! $post->body  !!}</div>
+            <div class="row mb-4">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="post/{{ $post->slug }}">{{ $post->title }}</a></h5>
+                            <p class="card-text" style="overflow:hidden;white-space: nowrap;text-overflow:ellipsis">{!! $post->body  !!}</p>
+                            <a href="post/{{ $post->slug }}" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                    <i style="font-size:small;color:grey">by <span style="color:black">{{ $post->user->first_name }}</span> {{ $post->created_at->diffForHumans() }}</i>
+                </div>
             </div>
         @endforeach
 
